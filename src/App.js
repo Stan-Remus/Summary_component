b1566e75360c27516ac2f82a1e27d4cc64136d5f
Robert Popescu <robert.popescu@radcom.ro>
2023-04-12 16:40:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import reaction from './icon-reaction.svg';
+import memory from './icon-memory.svg';
+import verbal from './icon-verbal.svg';
+import visual from './icon-visual.svg'
+import Card from '@mui/material/Card';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Card variant="outlined" className="container">
+          <div className="leftBox">
+            <h1>Your Result</h1>
+            <div className='circle'>
+              <h2>76</h2>
+              <h3>of 100</h3>
+            </div>
+            <div className='bold'>Great</div>
+            <p>You scored higher than 65% of the people who have taken these test.</p>
+          </div>
+          <div className='rightBox'>
+            <h1>Summary</h1>
+            <div className='rightContainer'>
+              <div className='row red'>
+                <div className='center'>
+                  <img src={reaction} alt=''/>
+                  <p>Reaction</p>
+                </div>
+                <p className='textBlack'><span>80</span> / 100</p>
+              </div>
+              <div className='row yellow'>
+                <div className='center'>
+                  <img src={memory} alt=''/>
+                  <p>Memory</p>
+                </div>
+                <p className='textBlack'> <span>92</span> / 100</p>
+              </div>
+              <div className='row green'>
+                <div className='center'>
+                  <img src={verbal}/>
+                  <p>Verbal</p>
+                </div>
+                <p className='textBlack'><span>61</span> / 100</p>
+              </div>
+              <div className='row blue'>
+                <div className='center'>
+                  <img src={visual}/>
+                  <p>Visual</p>
+                </div>
+                <p className='textBlack'><span>72</span> / 100</p>
+              </div>
+            </div>
+            <div className='buttonContainer'>
+              <button className='button'>Continue</button>
+            </div>
+          </div>         
+      </Card>
     </div>
   );
 }
